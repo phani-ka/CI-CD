35e22bccbf9a8c58a6663f97d49e9892d0687887
azuredevops <kphani7@outlook.com>
2022-09-19 10:37:24 +0530

    added the top command

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,7 +6,7 @@ pipeline {
 
             steps {
                
-                    sh 'ls -al  && java --version && top'
+                    sh 'terraform --version'
                
             }
         }
@@ -15,7 +15,7 @@ pipeline {
 
         stage('Deployment Stage') {
             steps {
-                 sh 'terraform --version'
+                 sh 'terraform init'
             }
         }
     }
---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.23.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+# Create a resource group
+resource "azurerm_resource_group" "RG" {
+  name     = "RG-resources"
+  location = "West Europe"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "RG" {
+  name                = "RG-network"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  address_space       = ["10.0.0.0/16"]
